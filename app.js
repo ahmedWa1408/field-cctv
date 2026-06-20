17b951b90b3f4c4c1735c8dde9033a8cf91076ff
ahmedWa1408 <aa1229550@gmail.com>
2026-06-20 04:59:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,85 +1,116 @@
-const plans = {
-  4: {
-    route: "مسار عنيزة والمذنب",
-    sites: [
-      "BRDIT169",
-      "QSMSM050",
-      "QSMSM051"
-    ]
-  }
-};
-
 function searchPlan() {
 
-  const plan = document.getElementById("planNumber").value;
+  const plan = document.getElementById("planNumber").value.trim();
   const table = document.getElementById("sitesTable");
 
   table.innerHTML = "";
 
   if (!plans[plan]) {
+
     document.getElementById("routeName").value = "";
+
     alert("رقم الخطة غير موجود");
+
     return;
+
   }
 
   document.getElementById("routeName").value = plans[plan].route;
 
   let html = `
-  <table border="1" style="width:100%;border-collapse:collapse;text-align:center">
-  <tr>
-      <th>رمز الموقع</th>
-      <th>XML</th>
-      <th>حالة الموقع</th>
-      <th>من</th>
-      <th>إلى</th>
-      <th>صورة العطل</th>
-      <th>ملاحظات</th>
-  </tr>
-  `;
+
+<table class="sites-table">
+
+<tr>
+
+<th>رمز الموقع</th>
+<th>نوع الوحدة</th>
+<th>📍 الموقع</th>
+<th>XML</th>
+<th>حالة الموقع</th>
+<th>من</th>
+<th>إلى</th>
+<th>📷 صور</th>
+<th>ملاحظات</th>
+
+</tr>
+
+`;
 
   plans[plan].sites.forEach(site => {
 
     html += `
-    <tr>
 
-      <td>${site}</td>
+<tr>
 
-      <td>
-        <select>
-          <option>نعم</option>
-          <option>لا</option>
-        </select>
-      </td>
+<td>${site.code}</td>
 
-      <td>
-        <select>
-          <option>يعمل</option>
-          <option>لا يعمل</option>
-          <option>يعمل ولا توجد مخالفات</option>
-        </select>
-      </td>
+<td>${site.storage}</td>
 
-      <td>
-        <input type="datetime-local">
-      </td>
+<td>
 
-      <td>
-        <input type="datetime-local">
-      </td>
+<a href="${site.map}" target="_blank">
 
-      <td>
-        <input type="file" accept="image/*">
-      </td>
+📍 فتح الموقع
 
-      <td>
-        <input type="text">
-      </td>
+</a>
 
-    </tr>
-    `;
+</td>
+
+<td>
+
+<label><input type="radio" name="xml_${site.code}"> نعم</label>
+
+<label><input type="radio" name="xml_${site.code}"> لا</label>
+
+</td>
+
+<td>
+
+<select>
+
+<option>يعمل</option>
+
+<option>لا يعمل</option>
+
+<option>يعمل ولا توجد مخالفات</option>
+
+</select>
+
+</td>
+
+<td>
+
+<input type="datetime-local">
+
+</td>
+
+<td>
+
+<input type="datetime-local">
+
+</td>
+
+<td>
+
+<input type="file" accept="image/*" multiple>
+
+</td>
+
+<td>
+
+<input type="text">
+
+</td>
+
+</tr>
+
+`;
+
   });
 
   html += "</table>";
 
   table.innerHTML = html;
+
 }
