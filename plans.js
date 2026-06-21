f77eeaf44a159110154341424a812a8b941eca12
ahmedWa1408 <aa1229550@gmail.com>
2026-06-21 12:11:46 +0300

    Refactor plans.js to update routes and sites

--- plans.js
+++ plans.js
@@ -1,59 +1,148 @@
 const plans = {
 
-  1: {
-    route: "عنيزة والمذنب",
-    sites: [
-      {
-        code: "QSMSM204",
-        storage: "فلاش",
-        map: "https://maps.app.goo.gl/rD6qtCLzpYay22AN6?g_st=ipc"
-      },
-      {
-        code: "QSMSM205",
-        storage: "فلاش",
-        map: "https://maps.app.goo.gl/rD6qtCLzpYay22AN6?g_st=ipc"
-      },
-      {
-        code: "QSMSM204-205",
-        storage: "3 فلاشات",
-        map: "https://maps.app.goo.gl/rD6qtCLzpYay22AN6?g_st=ipc"
-      },
-      {
-        code: "113",
-        storage: "فلاش",
-        map: "https://maps.app.goo.gl/ScMENYkk7k7s62KR7?g_st=iw"
-      },
-      {
-        code: "114",
-        storage: "فلاش",
-        map: "https://maps.app.goo.gl/ScMENYkk7k7s62KR7?g_st=iw"
-      }
-    ]
-  },
+1:{
 
-  2: {
-    route: "حائل",
-    sites: []
-  },
+route:"مسار 1 - عنيزة والمذنب",
 
-  3: {
-    route: "أم سدرة",
-    sites: []
-  },
+sites:[
 
-  4: {
-    route: "البدايع",
-    sites: []
-  },
+{
+code:"QSMSM204",
+storage:"فلاش",
+map:"https://maps.app.goo.gl/rD6qtCLzpYay22AN6?g_st=ipc"
+},
 
-  5: {
-    route: "قبة",
-    sites: []
-  },
+{
+code:"QSMSM205",
+storage:"فلاش",
+map:"https://maps.app.goo.gl/rD6qtCLzpYay22AN6?g_st=ipc"
+},
 
-  6: {
-    route: "شري الجديد",
-    sites: []
-  }
+{
+code:"QSMSM204-205",
+storage:"3 فلاشات",
+map:"https://maps.app.goo.gl/rD6qtCLzpYay22AN6?g_st=ipc"
+},
+
+{
+code:"113",
+storage:"فلاش",
+map:"https://maps.app.goo.gl/ScMENYkk7k7s62KR7?g_st=iw"
+},
+
+{
+code:"114",
+storage:"فلاش",
+map:"https://maps.app.goo.gl/ScMENYkk7k7s62KR7?g_st=iw"
+},
+
+{
+code:"115",
+storage:"فلاش",
+map:""
+},
+
+{
+code:"116",
+storage:"فلاش",
+map:""
+},
+
+{
+code:"117",
+storage:"فلاش",
+map:""
+},
+
+{
+code:"118",
+storage:"فلاش",
+map:""
+},
+
+{
+code:"119",
+storage:"فلاش",
+map:""
+},
+
+{
+code:"120",
+storage:"فلاش",
+map:""
+},
+
+{
+code:"121",
+storage:"فلاش",
+map:""
+},
+
+{
+code:"122",
+storage:"فلاش",
+map:""
+},
+
+{
+code:"123",
+storage:"فلاش",
+map:""
+},
+
+{
+code:"124",
+storage:"فلاش",
+map:""
+}
+
+]
+},2:{
+
+route:"مسار 2 - حائل",
+
+sites:[
+
+]
+
+},
+
+3:{
+
+route:"مسار 3 - أم سدرة",
+
+sites:[
+
+]
+
+},
+
+4:{
+
+route:"مسار 4 - البدايع",
+
+sites:[
+
+]
+
+},5:{
+
+route:"مسار 5 - قبة",
+
+sites:[
+
+]
+
+},
+
+6:{
+
+route:"مسار 6 - شري الجديد",
+
+sites:[
+
+]
+
+}
 
 };
