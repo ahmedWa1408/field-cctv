40b4ebf6b46f11f0b39f8438168a65f18d23f8a8
ahmedWa1408 <aa1229550@gmail.com>
2026-06-21 07:39:06 +0300

    Add event handlers for buttons and modify alerts

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
 window.onload = function () {
-    alert("تم تحميل JavaScript");
 
     setTimeout(function () {
-        alert("بعد ثانيتين");
 
         document.getElementById("splashScreen").style.display = "none";
         document.getElementById("welcomeScreen").classList.remove("hidden");
+
     }, 2000);
+
+    document.getElementById("startBtn").onclick = function () {
+
+        document.getElementById("welcomeScreen").classList.add("hidden");
+        document.getElementById("homePage").classList.remove("hidden");
+
+    };
+
+    document.getElementById("searchBtn").onclick = function () {
+
+        let number = document.getElementById("planNumber").value;
+
+        if (plans[number]) {
+
+            document.getElementById("routeName").value = plans[number].route;
+
+            document.getElementById("historyBtn").style.display = "block";
+            document.getElementById("missionBtn").style.display = "block";
+
+        } else {
+
+            alert("رقم الخطة غير موجود");
+
+        }
+
+    };
+
+    document.getElementById("missionBtn").onclick = function () {
+
+        window.location.href = "mission.html";
+
+    };
+
+    document.getElementById("historyBtn").onclick = function () {
+
+        window.location.href = "history.html";
+
+    };
+
 };
