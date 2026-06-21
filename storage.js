709ebd74642ac576870d75caa6e97405e37b61fd
ahmedWa1408 <aa1229550@gmail.com>
2026-06-21 11:00:38 +0300

    Implement auto-save for currentPlan
    Add auto-save functionality for currentPlan on page unload and every 20 seconds.

--- storage.js
+++ storage.js
@@ -1,1 +1,49 @@
+//======================================
+// حفظ عند إغلاق الصفحة
+//======================================
 
+window.addEventListener(
+
+"beforeunload",
+
+function(){
+
+    if(
+
+    typeof currentPlan!="undefined"
+
+    ){
+
+        autoSaveMission(currentPlan);
+
+    }
+
+}
+
+);
+
+
+
+//======================================
+// حفظ كل 20 ثانية
+//======================================
+
+setInterval(function(){
+
+    if(
+
+    typeof currentPlan!="undefined"
+
+    ){
+
+        autoSaveMission(currentPlan);
+
+    }
+
+},20000);
+
+
+
+//======================================
+// نهاية storage.js
+//======================================
